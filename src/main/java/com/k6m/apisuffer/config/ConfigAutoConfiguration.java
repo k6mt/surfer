eb1f5f77d30dbcf060148de9d8c371a0e2db970f
KzeroJun <rladudwns164@gmail.com>
2025-04-24 12:57:57 +0900

    feat : add auto setup of config module

--- src/main/java/com/k6m/apisuffer/config/ConfigAutoConfiguration.java
+++ src/main/java/com/k6m/apisuffer/config/ConfigAutoConfiguration.java
@@ -4,10 +4,12 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.Import;
 
 @EnableConfigurationProperties(ConfigProperties.class)
 @ConditionalOnProperty(name = ConfigProperties.PREFIX + ".enabled", matchIfMissing = true)
 @Configuration(proxyBeanMethods = false)
+@Import({ConfigAPIThreadTracker.class})
 @EnableAspectJAutoProxy
 public class ConfigAutoConfiguration {
 
