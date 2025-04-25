a65ae4f4536a2ef8fb0dc292f7d100585a6708b4
InJoo <jjoonior97@gmail.com>
2025-04-25 16:25:59 +0900

    feat: using MethodInterceptor

---
+++ src/main/java/com/k6m/surfer/SurferAutoConfiguration.java
@@ -0,0 +1,37 @@
+package com.k6m.surfer;
+
+import org.springframework.aop.Advisor;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.Bean;
+
+@AutoConfiguration
+public class SurferAutoConfiguration {
+
+  @Bean
+  public Advisor traceAdvisor() {
+    AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+    String basePackage = detectMainPackage();
+
+    String expression = basePackage.isBlank()
+        ? "execution(public * *(..))"
+        : String.format("execution(public * %s..*(..))", basePackage);
+
+    pointcut.setExpression(expression);
+
+    return new DefaultPointcutAdvisor(pointcut, new SurferMethodInterceptor(basePackage));
+  }
+
+
+  private String detectMainPackage() {
+    for (StackTraceElement element : Thread.currentThread().getStackTrace()) {
+      if (element.getMethodName().equals("main")) {
+        String className = element.getClassName();
+        int lastDot = className.lastIndexOf('.');
+        return lastDot > 0 ? className.substring(0, lastDot) : "";
+      }
+    }
+    return "";
+  }
+}
