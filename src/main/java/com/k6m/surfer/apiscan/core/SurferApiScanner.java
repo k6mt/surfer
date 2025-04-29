5df37039e593c04699d499c3ad2128c1e1861ab4
김지한 <wlgks7460@naver.com>
2025-04-29 16:31:15 +0900

    feature/ApiAnalyzer : Enhance handling of all parameters (RequestBody, PathVariable, RequestParam, RequestHeader)

--- src/main/java/com/k6m/surfer/apiscan/core/SurferApiScanner.java
+++ src/main/java/com/k6m/surfer/apiscan/core/SurferApiScanner.java
@@ -1,26 +1,33 @@
-package com.k6m.surfer;
+package com.k6m.surfer.apiscan.core;
 
+import jakarta.annotation.PostConstruct;
 import org.reflections.scanners.Scanners;
 import org.reflections.util.ConfigurationBuilder;
-import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 import org.reflections.Reflections;
 import org.springframework.web.bind.annotation.*;
 
 
 import java.lang.reflect.Method;
-import java.util.Set;
+import java.util.*;
 
-@Component
 public class SurferApiScanner {
 
     private final String basePackage;
+
+    private final Map<String, Method> apiMappingCache = new HashMap<>();
+
 
     public SurferApiScanner(String basePackage){
         this.basePackage = basePackage;
     }
 
-    public void apiScan(){
+    public Map<String, Method> getApiMappingCache(){
+        return apiMappingCache;
+    }
+
+    public List<Map<String, String>> apiScan(){
+        List<Map<String, String>> apis = new ArrayList<>();
         try{
             System.out.println("[ApiScanner] Scanning basePackage: " + basePackage);
 
@@ -52,13 +59,24 @@ public class SurferApiScanner {
                     if (methodPath != null && httpMethod != null) {
                         String fullPath = (basePath + methodPath).replaceAll("//+", "/");
                         System.out.printf("[%-6s] %s -> %s#%s%n", httpMethod, fullPath, controller.getSimpleName(), method.getName());
+                        Map<String, String> apiInfo = new HashMap<>();
+                        apiInfo.put("url", fullPath);
+                        apiInfo.put("method", httpMethod);
+                        apis.add(apiInfo);
+                        /**
+                         * Cache the mapping between (HTTP method + full URL) and the corresponding Java Method
+                         */
+                        apiMappingCache.put(httpMethod + ":" + fullPath, method);
                     }
                 }
             }
+
         }catch (Exception e){
             System.err.println("[ApiScanner] Error during scanning: " + e.getMessage());
             e.printStackTrace();
         }
+
+        return apis;
     }
 
     private String getPath(Method method) {
